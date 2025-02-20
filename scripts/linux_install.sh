d5b020dd453ff2eff0ac35e27f1fb395f3b51b65
Starry03 <andrea.starrantino@gmail.com>
2025-02-20 20:34:29 +0100

    Remove obsolete changes.log and icons.txt files; migrate installation scripts to new storage structure

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -9,7 +9,6 @@ sudo chmod +x /usr/local/bin/list
 # dependencies
 sudo mkdir /usr/local/share/list
 sudo rm /usr/local/share/list/*
-sudo cp ./icons.txt /usr/local/share/list/
-sudo cp ./changes.log /usr/local/share/list/
+sudo cp ./storage/* /usr/local/share/list/
 
 echo "Installed list to /usr/local/bin"
